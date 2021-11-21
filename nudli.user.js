80cbe6a10d3259b1f3f333fe10e781322559f34c
T. Boglárka <76449208+boglarkla@users.noreply.github.com>
2021-11-21 23:19:16 +0100

    Update nudli.user.js

--- nudli.user.js
+++ nudli.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NudliScript
 // @namespace    http://tampermonkey.net/
-// @version      0.6.6.1.1
+// @version      0.6.6.2
 // @description  Pár változtatás just for fun
 // @author       boglarkla
 // @match        *://elearning.uni-obuda.hu/main/*
@@ -45,6 +45,8 @@
       var bgwhite = document.getElementsByClassName("bg-white"); 
       for(var i = 0; i < bgwhite.length; i++){
         bgwhite[i].setAttribute('style', 'background-color: black !important');
+        bgwhite[i].style.cssText = 'background-color: black !important'
+        // document.getElementById("player").style.cssText = 'visibility:visible !important'
       }
       
       // $(".bg-white").removeAttr("style");
@@ -147,9 +149,7 @@
             {
                 links[i].style.color = "#ffffff";  
             }
-        } 
-      
-      $(".bg-white").removeAttr("style");
+        }
       
     }
 )();
